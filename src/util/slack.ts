f18627a8614be02f9857df6baec57785d172e107
Rade58 <bajic.rade2@gmail.com>
2023-08-01 15:06:09 +0200

    generic-6

---
+++ src/util/slack.ts
@@ -0,0 +1,17 @@
+export function slackApi(
+  endpoint: SlackApiEndpoint,
+  body: SlackApiRequestBody
+) {
+  //
+  //
+  return fetch(`https://slack.com/api/${endpoint}`, {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${process.env.SLACK_BOT_OAUTH_TOKEN}`,
+      'Content-Type': 'application/json; charset=utf-8',
+    },
+    body: JSON.stringify(body),
+  });
+  //
+  //
+}
